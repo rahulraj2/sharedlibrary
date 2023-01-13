5af073627c843278f301c7ced611ee1ac1143df6
Rahul Raj <Rahulraj21017@gmail.com>
2023-01-13 15:01:27 +0400

    sending from map

--- vars/build_utils.groovy
+++ vars/build_utils.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def build(Map config){
+def build(Map config = [:]){
     println config.ENV_APPLICATION_NAME
     println config.ENV_NAMESPACE_NAME
     echo "Hello Pipeline"
